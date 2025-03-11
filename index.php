730acbb1e14f7e922018e412282f71a03d55edcb
timoruth02 <timoruth81@gmail.com>
2025-03-11 11:25:55 -0500

    Inicio de proyecto

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Maqueta web</title>
+</head>
+<body>
+    <header>
+        <nav class="navegacion">
+            <a href="#">Inicio</a>
+            <a href="#">Nosotros</a>
+            <a href="#">Proyectos</a>
+            <a href="#">Contacto</a>
+        </nav>
+        <div class="header__contenido">
+            <img src="https://picsum.photos/id/684" alt="">
+        </div>
+    </header>
+</body>
+</html>
